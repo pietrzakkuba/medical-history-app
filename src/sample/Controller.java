4c8c7407a84dc567152485c4e71c3194ccc0d0cb
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-06-02 14:24:16 +0200

    main view create

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+    public void add(ActionEvent actionEvent) {
+    }
+
+    public void modify(ActionEvent actionEvent) {
+    }
+
+    public void delete(ActionEvent actionEvent) {
+    }
+
+    public void filtering(KeyEvent keyEvent) {
+    }
+
+    public void exit(ActionEvent actionEvent) {
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,21 +6,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.client.ServerValidationModeEnum;
-import ca.uhn.fhir.rest.client.IGenericClient;
-import ca.uhn.fhir.util.BundleUtil;
-import org.hl7.fhir.dstu3.model.*;
-import org.hl7.fhir.instance.model.api.IBaseBundle;
-import org.hl7.fhir.instance.model.api.IBaseResource;
+//import ca.uhn.fhir.context.FhirContext;
+//import ca.uhn.fhir.rest.client.ServerValidationModeEnum;
+//import ca.uhn.fhir.rest.client.IGenericClient;
+//import ca.uhn.fhir.util.BundleUtil;
+//import org.hl7.fhir.dstu3.model.*;
+//import org.hl7.fhir.instance.model.api.IBaseBundle;
+//import org.hl7.fhir.instance.model.api.IBaseResource;
 
 public class Main extends Application {
+
+//    private Stage mainStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Medical History");
+
         primaryStage.show();
     }
 
